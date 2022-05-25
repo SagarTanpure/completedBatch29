0c46e97820b13ded21a6ec60a344634b6f4c1857
SagarTanpure <tanpuresagar0@gmail.com>
2022-05-25 06:41:58 -0700

    testing
    tedting batch completed sucessfully completed

---
+++ Jan29Batch/src/testingBatch/TestingBatchComplte.java
@@ -0,0 +1,10 @@
+package testingBatch;
+
+public class TestingBatchComplte {
+
+	public static void main(String[] args) {
+		 System.out.println("Testing batch all are sucessfully jone");
+
+	}
+
+}
